373286d002308720a4372075cd7b8322fe814767
maguangcan <maguangcan@bloks.com>
2023-11-08 12:24:43 +0800

    v0.0.1

---
+++ example/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,16 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:flutter_prompt/flutter_prompt_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FlutterPromptWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
